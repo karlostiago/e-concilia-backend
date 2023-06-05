667dd007f12a3f9c260e4fae0c4a088d327308ed
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-06-05 18:51:14 -0300

    feat: criando arquivo de configuracao para rodar no heroku

--- src/main/java/com/ctsousa/econcilia/service/impl/CnpjServiceImpl.java
+++ src/main/java/com/ctsousa/econcilia/service/impl/CnpjServiceImpl.java
@@ -6,17 +6,31 @@ import com.ctsousa.econcilia.service.CnpjService;
 import com.ctsousa.econcilia.validation.CnpjValidator;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class CnpjServiceImpl implements CnpjService {
 
     private final ReceitaWS receitaWS;
 
+    private final Map<String, DadosCnpjJson> cache;
+
     public CnpjServiceImpl(ReceitaWS receitaWS) {
         this.receitaWS = receitaWS;
+        this.cache = new HashMap<>();
     }
 
     public DadosCnpjJson buscarCNPJ (final String cnpj) {
         new CnpjValidator(cnpj).validar();
-        return this.receitaWS.consultarDadosCNPJ(cnpj);
+
+        DadosCnpjJson dadosCnpjJson = cache.get(cnpj);
+
+        if (dadosCnpjJson == null) {
+            dadosCnpjJson = this.receitaWS.consultarDadosCNPJ(cnpj);
+            cache.put(cnpj, dadosCnpjJson);
+        }
+
+        return dadosCnpjJson;
     }
 }
