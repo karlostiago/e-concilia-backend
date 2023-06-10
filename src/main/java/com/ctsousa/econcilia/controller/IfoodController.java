85f3e4cdb50a528c87e8f7997d7eaab7d65c948a
Yuri Ravik <69877264+yuriravik@users.noreply.github.com>
2023-06-09 22:48:33 -0300

    busca de comerciantes e taxas de manutenção (#3)
    * busca de comerciantes e taxas de manutenção
    
    * melhoria chamada token

--- src/main/java/com/ctsousa/econcilia/controller/IfoodController.java
+++ src/main/java/com/ctsousa/econcilia/controller/IfoodController.java
@@ -1,8 +1,10 @@
 package com.ctsousa.econcilia.controller;
 
+import com.ctsousa.econcilia.model.ifood.Comerciante;
 import com.ctsousa.econcilia.model.ifood.DetalhesPedido;
-import com.ctsousa.econcilia.model.ifood.token.TokenAcesso;
 import com.ctsousa.econcilia.model.ifood.Pedido;
+import com.ctsousa.econcilia.model.ifood.TaxasManutencao;
+import com.ctsousa.econcilia.model.ifood.token.TokenAcesso;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/ifood")
 public class IfoodController {
 
     private static final String GRANT_TYPE = "grantType";
@@ -34,42 +37,75 @@ public class IfoodController {
 
     @GetMapping("ordens")
     public List<Pedido> getPedidos() throws JsonProcessingException {
-        String urlIfoodOrdens = "https://merchant-api.ifood.com.br/order/v1.0";
+        String urlIfoodOrdens = "https://merchant-api.ifood.com.br/order/v1.0/events:polling";
 
         String token = getTokenAcesso().getTokenAcesso();
 
 
         List<Pedido> ordens = webClient.get()
-                .uri(urlIfoodOrdens+"/events:polling")
+                .uri(urlIfoodOrdens)
                 .accept(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer "+token)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
                 .bodyToMono(List.class).block();
 
+
+        if (ordens == null) {
+            return null;
+        }
         return ordens;
     }
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
-    }
+    @GetMapping("/comerciantes")
+    public List<Comerciante> getComerciantes() {
+        String urlComerciante = "https://merchant-api.ifood.com.br/merchant/v1.0/merchants";
 
-    public TokenAcesso getTokenAcesso(){
-        String urlAutenticationIfood = "https://merchant-api.ifood.com.br/authentication/v1.0/oauth/token";
+        String token = getTokenAcesso().getTokenAcesso();
 
-        TokenAcesso tokenAcesso = webClient.post()
-                .uri(urlAutenticationIfood + "?"+GRANT_TYPE+"="+"client_credentials"+"&"+CLIEN_ID+"="+"9312b324-4b36-451f-b0ce-7671b8641751"+"&"+CLIEN_SECRET+"="+"llrirsr9pyc9rcugny4amqkol3m6s68qtmihaeif0puf7z7t64uy9ajy8l52n0klf0ijzu0ksn7ohg9r586xyzius7f11pze1g0"+"&"+AUTHORIZATION_CODE+"="+"&"+AUTHORIZATION_CODE_VERIFIER+"="+"&"+REFRESH_TOKEN+"=")
-                .contentType(MediaType.valueOf("application/x-www-form-urlencoded"))
+        List<Comerciante> comerciante = webClient.get()
+                .uri(urlComerciante)
                 .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
-                .bodyToMono(TokenAcesso.class).block();
+                .bodyToMono(List.class).block();
 
-        return tokenAcesso;
+        if (comerciante == null) {
+            return null;
+        }
+
+        return comerciante;
 
     }
 
-    @GetMapping("/detalhes")
-    public DetalhesPedido getDetalhesPedido(String ordemId){
+    @GetMapping("/taxas_manutencao")
+    public List<TaxasManutencao> getTaxasManutencao(String idComerciante, String periodo) {
+        String urlTaxasManutencao = "https://merchant-api.ifood.com.br/financial/v2.0/merchants/";
+        String endpoint = urlTaxasManutencao + idComerciante + "/maintenanceFees";
+
+        String token = getTokenAcesso().getTokenAcesso();
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(endpoint)
+                .queryParam("periodId", periodo);
+
+        String url = uriBuilder.toUriString();
+
+        List<TaxasManutencao> taxasManutencao = webClient.get()
+                .uri(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .retrieve()
+                .bodyToMono(List.class).block();
+
+        if (taxasManutencao == null) {
+            return null;
+        }
+
+        return taxasManutencao;
+
+    }
+
+    @GetMapping("/detalhes_pedido")
+    public DetalhesPedido getDetalhesPedido(String ordemId) {
         String urlIdPedido = "https://merchant-api.ifood.com.br/order/v1.0/orders/";
 
         String token = getTokenAcesso().getTokenAcesso();
@@ -77,11 +113,39 @@ public class IfoodController {
         DetalhesPedido detalhesPedido = webClient.get()
                 .uri(urlIdPedido + ordemId)
                 .accept(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer "+token)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
                 .bodyToMono(DetalhesPedido.class).block();
 
+        if (detalhesPedido == null) {
+            return null;
+        }
+
         return detalhesPedido;
+
+    }
+
+    public TokenAcesso getTokenAcesso() {
+        String urlAutenticationIfood = "https://merchant-api.ifood.com.br/authentication/v1.0/oauth/token";
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(urlAutenticationIfood)
+                .queryParam(GRANT_TYPE, "client_credentials")
+                .queryParam(CLIEN_ID, "9312b324-4b36-451f-b0ce-7671b8641751")
+                .queryParam(CLIEN_SECRET, "llrirsr9pyc9rcugny4amqkol3m6s68qtmihaeif0puf7z7t64uy9ajy8l52n0klf0ijzu0ksn7ohg9r586xyzius7f11pze1g0")
+                .queryParam(AUTHORIZATION_CODE, "")
+                .queryParam(AUTHORIZATION_CODE_VERIFIER, "")
+                .queryParam(REFRESH_TOKEN, "");
+
+        String url = uriBuilder.toUriString();
+
+        TokenAcesso tokenAcesso = webClient.post()
+                .uri(url)
+                .contentType(MediaType.valueOf("application/x-www-form-urlencoded"))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(TokenAcesso.class).block();
+
+        return tokenAcesso;
 
     }
 
