3cd9fc224981117fb079c4699d1c2b7c01efa31b
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-01-27 01:30:25 -0300

    feat: removendo importes nao utilizados e alterando para 8 pool maximo de conexoes

--- src/main/java/com/ctsousa/econcilia/resource/SegurancaResource.java
+++ src/main/java/com/ctsousa/econcilia/resource/SegurancaResource.java
@@ -4,13 +4,11 @@ package com.ctsousa.econcilia.resource;
 import com.ctsousa.econcilia.mapper.impl.UsuarioMapper;
 import com.ctsousa.econcilia.model.Usuario;
 import com.ctsousa.econcilia.model.dto.SegurancaDTO;
-import com.ctsousa.econcilia.model.dto.UsuarioDTO;
 import com.ctsousa.econcilia.service.SegurancaService;
-import com.ctsousa.econcilia.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
